17d395d3e5c82c4cebd3302c9ebb0eb122de2440
nhule90 <lehoanhu90@gmail.com>
2023-09-03 22:59:12 -0500

    app files

---
+++ src/components/detailComponents/RecipeCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./RecipeCard.module.css";
+import { useNavigate } from "react-router-dom";
+
+const RecipeCard = ({ recipe }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/recipe/${recipe.recipe_id}`);
+  };
+  return (
+    <div className={styles.recipe_card}>
+      <div>
+        <div className={styles.recipe_img_container}>
+          <img src={recipe.image_url} alt="recipe_picture" />
+        </div>
+        <h3>{recipe.recipe_name}</h3>
+      </div>
+      <button className="blue-btn" onClick={handleClick}>
+        See More
+      </button>
+    </div>
+  );
+};
+
+export default RecipeCard;
